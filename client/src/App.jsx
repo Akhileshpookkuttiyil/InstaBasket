ffc89d1ade70c177e07478b2a9b48cee9f1885fe
akhilesh <akhileshpookkuttiyil@gmail.com>
2025-04-16 20:50:40 +0530

    added new logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
