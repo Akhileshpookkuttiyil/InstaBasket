32b616ea5f9d4226ec7f5c8c9b61562321ac43e7
akhilesh <akhileshpookkuttiyil@gmail.com>
2025-04-25 08:17:52 +0530

    added searchbar enahancements

--- client/src/Components/Noresults.jsx
+++ client/src/Components/Noresults.jsx
@@ -2,24 +2,24 @@ import React from "react";
 
 const noResults = () => {
   return (
-    <div class="flex flex-col items-center justify-center text-sm max-md:px-4 mt-14">
-      <h1 class="text-8xl md:text-9xl font-bold text-primary">404</h1>
-      <div class="h-1 w-16 rounded bg-primary my-5 md:my-7"></div>
-      <p class="text-2xl md:text-3xl font-bold text-gray-800">Page Not Found</p>
-      <p class="text-sm md:text-base mt-4 text-gray-500 max-w-md text-center">
+    <div className="flex flex-col items-center justify-center text-sm max-md:px-4 mt-14">
+      <h1 className="text-8xl md:text-9xl font-bold text-primary">404</h1>
+      <div className="h-1 w-16 rounded bg-primary my-5 md:my-7"></div>
+      <p className="text-2xl md:text-3xl font-bold text-gray-800">Page Not Found</p>
+      <p className="text-sm md:text-base mt-4 text-gray-500 max-w-md text-center">
         The page you are looking for might have been removed, had its name
         changed, or is temporarily unavailable.
       </p>
-      <div class="flex items-center gap-4 mt-6">
+      <div className="flex items-center gap-4 mt-6">
         <a
           href="/"
-          class=" border border-primary px-7 py-2.5 text-gray-800 rounded-md active:scale-95 transition-all"
+          className=" border border-primary px-7 py-2.5 text-gray-800 rounded-md active:scale-95 transition-all"
         >
           Return Home
         </a>
         <a
           href="/"
-          class="border border-primary px-7 py-2.5 text-gray-800 rounded-md active:scale-95 transition-all"
+          className="border border-primary px-7 py-2.5 text-gray-800 rounded-md active:scale-95 transition-all"
         >
           Contact support
         </a>
